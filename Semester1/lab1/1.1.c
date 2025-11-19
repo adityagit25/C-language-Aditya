2a3d5f4a3365b729889903dcd2873d9dc72b6452
aditya <upesadityagit@gmail.com>
2025-11-19 23:37:53 +0530

    add 50 70

---
+++ Semester1/lab1/1.1.c
@@ -0,0 +1,11 @@
+/* Experiment 1: Installation, Environment Setup and starting
+with C language
+1. Write a C program to print “Hello World” */
+#include <stdio.h>
+int main() {
+ printf("Name - Aditya Dabral\nSAP iD - 590029459\nCourse-BCA\nBatch-6");
+    printf("\n-------------------------------------------------\n");
+
+printf("Hello World\n");
+return 0;
+}
